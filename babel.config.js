597e9152ab6bdbc1d50f596fc490d11ab0751c94
Simon <kierej@gmail.com>
2025-09-12 15:52:59 +0200

    error after reanimated

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = ["react-native-worklets/plugin"];
+  let plugins = ['react-native-worklets/plugin', 'react-native-reanimated/plugin'];
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
