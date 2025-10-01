f2be33bc66a3e0cb0df7f5ca1583210b5d042922
Simon <kierej@gmail.com>
2025-10-01 13:51:45 +0200

    saved

--- components/PAGE/generation/components/ParticleRenderer.tsx
+++ components/PAGE/generation/components/ParticleRenderer.tsx
@@ -12,6 +12,10 @@ interface ParticleRendererProps {
 // Shared Paint object - create once and reuse
 const sharedPaint = Skia.Paint()
 
+// Shared clip path - create once with unit radius, scale during use
+const clipPath = Skia.Path.Make()
+clipPath.addCircle(0, 0, 1) // Unit circle, will be scaled
+
 export default function ParticleRenderer({
   image,
   particlesShared,
@@ -21,19 +25,17 @@ export default function ParticleRenderer({
     const currentParticles = particlesShared.get()
     const { particleSize, stageWidth, stageHeight } = config
 
-    // Pre-calculate image rectangles once
+    // Pre-calculate image rectangles once per frame
     const srcRect = rect(0, 0, image.width(), image.height())
     const dstRect = rect(0, 0, stageWidth, stageHeight)
 
     return createPicture((canvas) => {
       currentParticles.forEach((particle) => {
-        // Create clip path for this particle
-        const clipPath = Skia.Path.Make()
-        clipPath.addCircle(0, 0, particleSize)
-
         canvas.save()
         canvas.translate(particle.x, particle.y)
+        canvas.scale(particleSize, particleSize) // Scale the unit circle to particle size
         canvas.clipPath(clipPath, ClipOp.Intersect, true)
+        canvas.scale(1 / particleSize, 1 / particleSize) // Undo scale for image
         canvas.translate(-particle.x, -particle.y)
         canvas.drawImageRect(image, srcRect, dstRect, sharedPaint)
         canvas.restore()
